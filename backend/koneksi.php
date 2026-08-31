8a35f9dca6a27c4d9a52aa9fd40e9601df259c19
lisaanrf-26 <lisaaanrf@gmail.com>
2026-08-31 10:38:47 +0700

    Update koneksi

--- backend/koneksi.php
+++ backend/koneksi.php
@@ -1,11 +1,11 @@
 <?php
-
 $host = "127.0.0.1";
+$port = 3307; // sesuai port MySQL yang jalan di XAMPP Control Panel kamu
 $user = "root";
 $pass = "";
 $db   = "inventory_db";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
